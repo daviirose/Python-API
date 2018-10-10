131b88ef74a56af470b944b093021576825dd258
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-10-10 12:03:20 -0400

    Added my PUT and DELETE endpoints

--- api.py
+++ api.py
@@ -11,5 +11,29 @@ def test():
 def returnAll():
     return jsonify({'languages' : languages})
 
-    if __name__ == '__main__':
-        app.run(debug=True, port=8080) #Runs app on port 8080 in debug mode
+@app.route('/lang/<string:name>', methods=['GET'])
+def returnOne(name):
+    lang = [language for language in languages if language['name'] == name]
+    return jsonify({'language' : lang[0]})
+
+@app.route('/lang', methods=['POST'])
+def addOne():
+    language = {'name' : request.json['name']}
+
+    languages.append(language)
+    return jsonify({'languages' : languages})
+
+@app.route('/lang/<string:name>', methods=['PUT'])
+def editOne(name):
+    lang = [language for language in languages if language['name'] == name]
+    lang[0]['names'] = request.json['name']
+    return jsonify({'language' : lang[0]})
+
+@app.route('/lang/<string:name>', methods=['DELETE'])
+def removeOne(name):
+    lang = [language for language in languages if language['name'] == name]
+    langauge.remove(lang[0])
+    return jsonify({'languages' : languages})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080) #Runs app on port 8080 in debug mode
